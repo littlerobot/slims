29ee2967f16260538ef899a1a379c16ec09b0e42
Stuart Chapman <github@stuart-chapman.com>
2014-09-20 15:40:57 +0100

    Add basic Raven authentication

---
+++ src/Cscr/SlimsBundle/Controller/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cscr\SlimsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login")
+     * @Template
+     */
+    public function loginAction()
+    {
+        return array();
+    }
+}
+ 
---
+++ src/Cscr/SlimsBundle/CscrSlimsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cscr\SlimsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CscrSlimsBundle extends Bundle
+{
+}
