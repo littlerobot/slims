c132541aa6c9e18fc6da462e11d787cb863933ea
Rob Hogan <rh389@cam.ac.uk>
2014-10-16 09:51:44 +0100

    Remove unnecessary listener

--- web/app/view/Viewport.js
+++ web/app/view/Viewport.js
@@ -10,31 +10,7 @@ Ext.define('App.view.Viewport', {
     items: [
         {
             xtype: 'tabpanel',
-            title: '<span id="title">Slims</span>',
-            listeners: {
-                afterrender: {
-                    fn: function() {
-
-                        console.log(Ext.StoreManager.lookup('User'));
-                        // after this tabpanel is drawn add a logout button and a wee welcome message with the user's name.
-                        var headers = Ext.select('#main div.x-tab-strip-wrap');
-                        if(!headers || headers.length === 0)
-                            throw new Error();
-
-                        var name = Ext.DomHelper.createDom({
-                            tag: 'span',
-                            id: 'tabpanel-rderame',
-                            html: 'Welcome<?php echo $id ? ", <b>".$id."</b>" : ""; ?>'
-                        });
-                        headers.appendChild(name);
-                        // hyperlink is added to the far-right (see css)
-                        var a = Ext.DomHelper.createDom({tag:'a', id:'tabpanel-logout-link', cls: 'slims-tabpanel-link', html: 'Log out' });
-                        // this should point to the raven logout url
-                        a.setAttribute('href','logout.php');
-                        headers.appendChild(a);
-                    }
-                }
-            }
+            title: '<span id="title">Slims</span>'
         }
     ]
 });
