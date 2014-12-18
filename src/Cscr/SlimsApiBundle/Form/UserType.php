b34ace0193930f42014ae7ca64622b7286186eca
Stuart Chapman <github@stuart-chapman.com>
2014-12-18 19:00:15 +0000

    User create and edit API #4
    * Controller actions.
    * UserType form definition.
    * User entity validation.

---
+++ src/Cscr/SlimsApiBundle/Form/UserType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cscr\SlimsApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text')
+            ->add('username', 'text')
+            ->add('research_group', 'entity', [
+                'class' => 'Cscr\SlimsApiBundle\Entity\ResearchGroup',
+                'property' => 'id',
+            ])
+            ->add('is_active', 'checkbox')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'      => 'Cscr\SlimsUserBundle\Entity\User',
+            'csrf_protection' => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'user';
+    }
+}
