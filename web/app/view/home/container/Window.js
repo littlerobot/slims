77783fbd30fbe2d6642dd56af7e7d4b7027d75c6
Alexey Lazarev <siralexlazarev@gmail.com>
2014-12-23 23:07:34 +0700

    Container form development. Rebuild

--- web/app/view/home/container/Window.js
+++ web/app/view/home/container/Window.js
@@ -4,13 +4,15 @@ Ext.define('Slims.view.home.container.Window', {
 
     requires: ['Ext.form.Panel'],
 
-    layout: 'hbox',
+    layout: 'fit',
     width: 800,
     height: 500,
+    resizable: false,
 
     requires: [
         'Ext.form.field.Checkbox',
         'Ext.form.RadioGroup',
+        'Ext.form.field.Number',
         'Ext.form.field.Radio'
     ],
 
@@ -19,111 +21,189 @@ Ext.define('Slims.view.home.container.Window', {
 
         this.items = [{
             xtype: 'form',
-            border: true,
-            bodyPadding: 10,
-            flex: 1,
-            height: '100%',
-            defaults: {
-                anchor: '100%',
-                labelWidth: 60
-            },
+            layout: 'hbox',
             items: [{
-                xtype: 'textfield',
-                fieldLabel: 'Name'
-            }, {
-                xtype: 'radiogroup',
-                layout: 'vbox',
-                items: [{
-                    boxLabel: 'Only holds other containers',
-                    name: 'holds_other_containers',
-                    inputValue: '2',
-                    checked: true
-                }, {
-                    boxLabel: 'Stored inside',
-                    name: 'holds_other_containers',
-                    inputValue: '1'
-                }]
-            }, {
                 xtype: 'panel',
-                html: '<center style="margin: 40px;"><h4>Containers tree</h4></center>',
-                border: true,
-                height: 150,
-                width: 300,
-                style: 'margin-left: 20px;'
-            }, {
-                xtype: 'radiogroup',
-                style: 'margin-top: 10px;',
-                layout: 'vbox',
-                labelAlign: 'top',
-                boxLabel: 'Belongs to',
+                bodyPadding: 10,
+                flex: 1,
+                height: '100%',
                 items: [{
-                    boxLabel: 'Nobody',
-                    name: 'belongs_to',
-                    inputValue: '',
-                    checked: true
+                    xtype: 'textfield',
+                    allowBlank: false,
+                    width: '100%',
+                    labelWidth: 40,
+                    fieldLabel: 'Name'
+                }, {
+                    xtype: 'radiogroup',
+                    layout: 'vbox',
+                    items: [{
+                        boxLabel: 'Only holds other containers',
+                        name: 'holds_other_containers',
+                        inputValue: '2',
+                        checked: true
+                    }, {
+                        boxLabel: 'Stored inside',
+                        name: 'holds_other_containers',
+                        inputValue: '1'
+                    }]
+                }, {
+                    xtype: 'panel',
+                    html: '<center style="margin: 40px;"><h4>Containers tree</h4></center>',
+                    border: true,
+                    height: 150,
+                    width: 300,
+                    bodyStyle: 'background: #C0C0C0;',
+                    style: 'margin-left: 20px;'
+                }, {
+                    xtype: 'radiogroup',
+                    style: 'margin-top: 10px;',
+                    layout: 'vbox',
+                    width: '100%',
+                    labelAlign: 'top',
+                    fieldLabel: 'Belongs to',
+                    items: [{
+                        boxLabel: 'Nobody',
+                        name: 'belongs_to',
+                        inputValue: '',
+                        checked: true
+                    }, {
+                        xtype: 'fieldcontainer',
+                        width: '100%',
+                        layout: 'hbox',
+                        items: [{
+                            xtype: 'radiofield',
+                            name: 'belongs_to',
+                            inputValue: '3'
+                        }, {
+                            xtype: 'combobox',
+                            style: 'margin-left: 5px;',
+                            flex: 1,
+                            editable: false,
+                            store: Ext.StoreMgr.get('researchGroups'),
+                            queryMode: 'local',
+                            displayField: 'name',
+                            valueField: 'id'
+                        }]
+                    }]
                 }, {
                     xtype: 'fieldcontainer',
-                    layout: 'hbox',
+                    layout: {
+                        type: 'hbox',
+                        align: 'middle'
+                    },
+                    style: 'margin-top: 10px;',
+                    width: '100%',
+                    labelWidth: 130,
+                    fieldLabel: 'Dimensions/capacity',
                     items: [{
-                        xtype: 'radiofield',
-                        name: 'belongs_to',
-                        inputValue: '3'
+                        xtype: 'numberfield',
+                        name: 'rowsCount',
+                        minValue: 0,
+                        hideAlign: true,
+                        flex: 1
                     }, {
-                        xtype: 'combobox',
-                        style: 'margin-left: 5px;',
+                        xtype: 'label',
+                        style: 'margin-left: 4px;',
+                        width: 50,
+                        text: 'rows by'
+                    }, {
+                        xtype: 'numberfield',
+                        name: 'columnsCount',
+                        minValue: 0,
+                        hideAlign: true,
                         flex: 1,
-                        editable: false,
-                        store: Ext.StoreMgr.get('researchGroups'),
-                        queryMode: 'local',
-                        displayField: 'name',
-                        valueField: 'id'
+                        labelAlign: 'right'
+                    }, {
+                        xtype: 'label',
+                        style: 'margin-left: 4px;',
+                        width: 50,
+                        text: 'columns'
+                    }]
+                }, {
+                    xtype: 'fieldcontainer',
+                    width: '100%',
+                    style: 'margin-top: 10px;',
+                    layout: {
+                        type: 'hbox',
+                        pack: 'end'
+                    },
+                    items: [{
+                        xtype: 'label',
+                        text: 'Total capacity:'
+                    }, {
+                        xtype: 'label',
+                        text: '0',
+                        style: 'margin-left: 4px;',
+                        name: 'totalCapacity'
                     }]
                 }]
             }, {
-                xtype: 'fieldcontainer',
-                layout: 'hbox',
-                style: 'margin-top: 10px;',
-                width: '100%',
-                labelWidth: 130,
-                fieldLabel: 'Dimensions/capacity',
+                xtype: 'panel',
+                bodyPadding: 10,
+                flex: 1,
+                height: '100%',
                 items: [{
-                    xtype: 'textfield',
-                    hideAlign: true,
-                    flex: 1
+                    xtype: 'radiogroup',
+                    layout: 'vbox',
+                    width: '100%',
+                    labelAlign: 'top',
+                    fieldLabel: 'Stores',
+                    items: [{
+                        boxLabel: 'Samples',
+                        name: 'stores',
+                        inputValue: '1',
+                        checked: true
+                    }, {
+                        boxLabel: 'Other containers',
+                        name: 'stores',
+                        inputValue: '2'
+                    }]
                 }, {
-                    xtype: 'label',
-                    style: 'margin-top: 4px; margin-left: 4px;',
-                    width: 50,
-                    text: 'rows by'
+                    xtype: 'textarea',
+                    width: '100%',
+                    fieldLabel: 'Comment',
+                    labelAlign: 'top',
+                    height: 250
                 }, {
-                    xtype: 'textfield',
-                    hideAlign: true,
-                    flex: 1,
-                    labelAlign: 'right'
-                }, {
-                    xtype: 'label',
-                    style: 'margin-top: 4px; margin-left: 4px;',
-                    width: 50,
-                    text: 'columns'
+                    xtype: 'numberfield',
+                    fieldLabel: 'Number of containers to create',
+                    labelAlign: 'top',
+                    width: 240,
+                    maxValue: 10,
+                    minValue: 1,
+                    value: 1
                 }]
             }]
-        }, {
-            xtype: 'form',
-            border: true,
-            bodyPadding: 10,
-            flex: 1,
-            height: '100%',
-            items: []
         }];
 
-        this.bbar = ['->', {
-            xtype: 'button',
+        this.bbar = [
+            '->', {
+            text: 'Save',
+            icon: '/resources/images/save.png',
             name: 'save',
-            text: 'Save'
+            scope: this,
+            handler: function() {
+                var container = this.record;
+
+                if (!this.down('form').getForm().isValid())
+                    return;
+
+                var formValues = this.down('form').getForm().getValues();
+
+                // TODO: Add data prepare here
+                if (!container) {
+                    container = Ext.create('Slims.model.Container', formValues);
+                } else {
+                    container.set(formValues);
+                }
+
+                this.fireEvent('save', container, this);
+            }
         }, '-', {
-            xtype: 'button',
-            name: 'cancel',
-            text: 'Cancel'
+            text: 'Cancel',
+            icon: '/resources/images/cancel.png',
+            scope: this,
+            handler: this.close
         }];
 
         this.callParent();
