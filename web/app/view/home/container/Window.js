78f1e6f6ce1c3fe0ecf3abedd43f71a0d35a4b76
Alexey Lazarev <siralexlazarev@gmail.com>
2015-01-02 12:15:20 +0700

    #7 Total capacity counter.

--- web/app/view/home/container/Window.js
+++ web/app/view/home/container/Window.js
@@ -20,6 +20,7 @@ Ext.define('Slims.view.home.container.Window', {
     ],
 
     initComponent: function() {
+        // TODO: new or edit title
         this.title = 'New container';
 
         this.items = [{
@@ -58,90 +59,11 @@ Ext.define('Slims.view.home.container.Window', {
                     displayField: 'name',
                     store: Ext.create('Slims.store.Containers'),
                     rootVisible: false
-                }, {
-                    xtype: 'radiogroup',
-                    style: 'margin-top: 10px;',
-                    layout: 'vbox',
-                    width: '100%',
-                    labelAlign: 'top',
-                    fieldLabel: 'Belongs to',
-                    items: [{
-                        boxLabel: 'Nobody',
-                        name: 'belongs_to',
-                        inputValue: '',
-                        checked: true
-                    }, {
-                        xtype: 'fieldcontainer',
-                        width: '100%',
-                        layout: 'hbox',
-                        items: [{
-                            xtype: 'radiofield',
-                            name: 'belongs_to',
-                            inputValue: '3'
-                        }, {
-                            xtype: 'combobox',
-                            style: 'margin-left: 5px;',
-                            flex: 1,
-                            emptyText: 'Select research group',
-                            editable: false,
-                            store: Ext.StoreMgr.get('researchGroups'),
-                            queryMode: 'local',
-                            displayField: 'name',
-                            valueField: 'id'
-                        }]
-                    }]
-                }, {
-                    xtype: 'fieldcontainer',
-                    layout: {
-                        type: 'hbox',
-                        align: 'middle'
-                    },
-                    style: 'margin-top: 10px;',
-                    width: '100%',
-                    labelWidth: 130,
-                    fieldLabel: 'Dimensions/capacity',
-                    items: [{
-                        xtype: 'numberfield',
-                        name: 'rowsCount',
-                        minValue: 0,
-                        hideAlign: true,
-                        flex: 1
-                    }, {
-                        xtype: 'label',
-                        style: 'margin-left: 4px;',
-                        width: 50,
-                        text: 'rows by'
-                    }, {
-                        xtype: 'numberfield',
-                        name: 'columnsCount',
-                        minValue: 0,
-                        hideAlign: true,
-                        flex: 1,
-                        labelAlign: 'right'
-                    }, {
-                        xtype: 'label',
-                        style: 'margin-left: 4px;',
-                        width: 50,
-                        text: 'columns'
-                    }]
-                }, {
-                    xtype: 'fieldcontainer',
-                    width: '100%',
-                    style: 'margin-top: 10px;',
-                    layout: {
-                        type: 'hbox',
-                        pack: 'end'
-                    },
-                    items: [{
-                        xtype: 'label',
-                        text: 'Total capacity:'
-                    }, {
-                        xtype: 'label',
-                        text: '0',
-                        style: 'margin-left: 4px;',
-                        name: 'totalCapacity'
-                    }]
-                }]
+                },
+                    this.getResearchGroupRadioCmp(),
+                    this.getDimensionsCmp(),
+                    this.getDimensionsTotalCmp()
+                ]
             }, {
                 xtype: 'panel',
                 bodyPadding: 10,
@@ -163,46 +85,9 @@ Ext.define('Slims.view.home.container.Window', {
                         name: 'stores',
                         inputValue: '2'
                     }]
-                },{
-                    xtype: 'container',
-                    layout: {
-                        type: 'hbox',
-                        align: 'middle'
-                    },
-                    items: [{
-                        xtype: 'panel',
-                        border: true,
-                        layout: 'fit',
-                        height: 24,
-                        width: 24,
-                        items: [{
-                            xtype: 'container',
-                            name: 'colorPalette',
-                            listeners: {
-                                afterrender: function(container) {
-                                    container.el.dom.removeChild(container.el.dom.childNodes[0]);
-                                }
-                            }
-                        }]
-                    }, {
-                        xtype: 'button',
-                        style: 'margin-left: 8px;',
-                        text: 'Select color',
-                        width: 110,
-                        name: 'colorButton',
-                        menu: [{
-                            xtype: 'colorpicker',
-                            value: 'FFFFFF',
-                            listeners: {
-                                scope: this,
-                                select: function(picker, selColor) {
-                                    picker.up('button[name=colorButton]').menu.hide();
-                                    this.down('container[name=colorPalette]').el.setStyle('background', '#'+selColor);
-                                }
-                            }
-                        }]
-                    }]
-                }, {
+                },
+                    this.getColorPickerCmp(),
+                {
                     xtype: 'textarea',
                     width: '100%',
                     fieldLabel: 'Comment',
@@ -221,7 +106,167 @@ Ext.define('Slims.view.home.container.Window', {
             }]
         }];
 
-        this.bbar = [
+        this.bbar = this.getBbarCmpArray();
+
+        this.callParent();
+    },
+
+    getResearchGroupRadioCmp: function() {
+        return {
+            xtype: 'radiogroup',
+            style: 'margin-top: 10px;',
+            layout: 'vbox',
+            width: '100%',
+            labelAlign: 'top',
+            fieldLabel: 'Belongs to',
+            items: [{
+                boxLabel: 'Nobody',
+                name: 'belongs_to',
+                inputValue: '',
+                checked: true
+            }, {
+                xtype: 'fieldcontainer',
+                width: '100%',
+                layout: 'hbox',
+                items: [{
+                    xtype: 'radiofield',
+                    name: 'belongs_to',
+                    inputValue: '3'
+                }, {
+                    xtype: 'combobox',
+                    style: 'margin-left: 5px;',
+                    flex: 1,
+                    emptyText: 'Select research group',
+                    editable: false,
+                    store: Ext.StoreMgr.get('researchGroups'),
+                    queryMode: 'local',
+                    displayField: 'name',
+                    valueField: 'id'
+                }]
+            }]
+        };
+    },
+
+    getDimensionsCmp: function() {
+        var updateTotal = function(cmp, val) {
+            var rowsCount = this.down('numberfield[name=rowsCount]').getValue() || 1;
+            var columnsCount = this.down('numberfield[name=columnsCount]').getValue() || 1;
+
+            this.down('label[name=totalCapacity]').setText(rowsCount*columnsCount);
+        };
+
+
+        return {
+            xtype: 'fieldcontainer',
+            layout: {
+                type: 'hbox',
+                align: 'middle'
+            },
+            style: 'margin-top: 10px;',
+            width: '100%',
+            labelWidth: 130,
+            fieldLabel: 'Dimensions/capacity',
+            items: [{
+                xtype: 'numberfield',
+                name: 'rowsCount',
+                minValue: 1,
+                value: 1,
+                listeners: {
+                    change: updateTotal,
+                    scope: this
+                },
+                flex: 1
+            }, {
+                xtype: 'label',
+                style: 'margin-left: 4px;',
+                width: 50,
+                text: 'rows by'
+            }, {
+                xtype: 'numberfield',
+                name: 'columnsCount',
+                value: 1,
+                minValue: 1,
+                flex: 1,
+                listeners: {
+                    change: updateTotal,
+                    scope: this
+                },
+                labelAlign: 'right'
+            }, {
+                xtype: 'label',
+                style: 'margin-left: 4px;',
+                width: 50,
+                text: 'columns'
+            }]
+        };
+    },
+
+    getDimensionsTotalCmp: function() {
+        return {
+            xtype: 'fieldcontainer',
+            width: '100%',
+            style: 'margin-top: 10px;',
+            layout: {
+                type: 'hbox',
+                pack: 'end'
+            },
+            items: [{
+                xtype: 'label',
+                text: 'Total capacity:'
+            }, {
+                xtype: 'label',
+                text: '0',
+                style: 'margin-left: 4px;',
+                name: 'totalCapacity'
+            }]
+        };
+    },
+
+    getColorPickerCmp: function() {
+        return {
+            xtype: 'container',
+            layout: {
+                type: 'hbox',
+                align: 'middle'
+            },
+            items: [{
+                xtype: 'panel',
+                border: true,
+                layout: 'fit',
+                height: 24,
+                width: 24,
+                items: [{
+                    xtype: 'container',
+                    name: 'colorPalette',
+                    listeners: {
+                        afterrender: function(container) {
+                            container.el.dom.removeChild(container.el.dom.childNodes[0]);
+                        }
+                    }
+                }]
+            }, {
+                xtype: 'button',
+                style: 'margin-left: 8px;',
+                text: 'Select color',
+                width: 110,
+                name: 'colorButton',
+                menu: [{
+                    xtype: 'colorpicker',
+                    value: 'FFFFFF',
+                    listeners: {
+                        scope: this,
+                        select: function(picker, selColor) {
+                            picker.up('button[name=colorButton]').menu.hide();
+                            this.down('container[name=colorPalette]').el.setStyle('background', '#'+selColor);
+                        }
+                    }
+                }]
+            }]
+        };
+    },
+
+    getBbarCmpArray: function() {
+        return [
             '->', {
             text: 'Save',
             icon: '/resources/images/save.png',
@@ -251,7 +296,5 @@ Ext.define('Slims.view.home.container.Window', {
             scope: this,
             handler: this.close
         }];
-
-        this.callParent();
     }
 });
