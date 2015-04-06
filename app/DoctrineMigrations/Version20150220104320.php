07ad75b7abfefdb8f08036cb997a7c95f0ba98da
Stuart Chapman <github@stuart-chapman.com>
2015-04-06 15:40:12 +0100

    Fix migration to allow column change with foreign key

--- app/DoctrineMigrations/Version20150220104320.php
+++ app/DoctrineMigrations/Version20150220104320.php
@@ -17,7 +17,14 @@ class Version20150220104320 extends AbstractMigration
             'Migration can only be executed safely on \'mysql\'.'
         );
 
+        $this->addSql('ALTER TABLE sample_type_attribute DROP FOREIGN KEY FK_2D88F7F0D5064105');
         $this->addSql('ALTER TABLE sample_type_attribute CHANGE sample_type_id sample_type_id INT NOT NULL');
+        $this->addSql('
+            ALTER TABLE sample_type_attribute
+              ADD CONSTRAINT FK_2D88F7F0D5064105
+                FOREIGN KEY (sample_type_id)
+                REFERENCES sample_type (id)
+        ');
     }
 
     public function down(Schema $schema)
@@ -27,6 +34,13 @@ class Version20150220104320 extends AbstractMigration
             'Migration can only be executed safely on \'mysql\'.'
         );
 
+        $this->addSql('ALTER TABLE sample_type_attribute DROP FOREIGN KEY FK_2D88F7F0D5064105');
         $this->addSql('ALTER TABLE sample_type_attribute CHANGE sample_type_id sample_type_id INT DEFAULT NULL');
+        $this->addSql('
+            ALTER TABLE sample_type_attribute
+              ADD CONSTRAINT FK_2D88F7F0D5064105
+                FOREIGN KEY (sample_type_id)
+                REFERENCES sample_type (id)
+        ');
     }
 }
