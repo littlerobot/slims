4d60596b5ca7d7033cfdf075ad9106ae0d4968c1
Stuart Chapman <github@stuart-chapman.com>
2015-07-26 16:31:31 +0100

    Add dummy filter route for testing - #47

--- src/Cscr/SlimsApiBundle/Controller/SamplesController.php
+++ src/Cscr/SlimsApiBundle/Controller/SamplesController.php
@@ -6,6 +6,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
 class SamplesController extends FOSRestController
 {
@@ -35,5 +36,49 @@ class SamplesController extends FOSRestController
             );
 
         return View::create($results);
+    }
+
+    /**
+     * @Rest\Route("/dummy")
+     */
+    public function dummyAction()
+    {
+        $content = <<<HEREDOC
+{
+  "success": true,
+  "data": [
+    {
+      "container": 5,
+      "containerName": "Floor 4",
+      "row": 7,
+      "column": 7,
+      "colour": "#451322",
+      "sampleType": 22,
+      "sampleTypeName": "Mouse Mickey",
+      "name": "Sample Name",
+      "sampleTemplate": 31,
+      "sampleTemplateName": "Template Name",
+      "attributes": [
+        {
+          "id": 12,
+          "label": "City",
+          "type": "text",
+          "value": "Los Angeles"
+        },
+        {
+          "id": 1,
+          "label": "Attached map",
+          "type": "file",
+          "filename": "Manufacturer information.pdf",
+          "mime_type": "application/pdf",
+          "value": "<base 64 encoded file content>"
+        }
+      ]
+    }
+  ]
+}
+HEREDOC;
+
+        return new Response($content);
     }
 }
