0ecbb40b3bbba7d92bf9b9c3610f9a4055ee2d80
Alexey Lazarev <siralexlazarev@gmail.com>
2015-08-14 20:39:20 +0700

    typecplumn development

--- web/app/ux/GridColumn.js
+++ web/app/ux/GridColumn.js
@@ -6,66 +6,70 @@
  *
 **/
 Ext.define('Slims.ux.GridColumn', {
-	extend: 'Ext.grid.column.Column',
-	xtype: 'typecolumn',
+    extend: 'Ext.grid.column.Column',
+    xtype: 'typecolumn',
 
-	// one of ['brief-text', 'long-text', colour', 'document', 'option', 'date', 'user']
-	type: 'brief-text',
-	initComponent: function() {
-		this.selectRendererByType();
+    // one of ['brief-text', 'long-text', colour', 'document', 'option', 'date', 'user']
+    type: 'brief-text',
+    initComponent: function() {
+        this.selectRendererByType();
 
-		this.callParent();
-	},
+        this.callParent();
+    },
 
-	selectRendererByType: function() {
-		var rendererFn = Ext.emptyFn;
+    selectRendererByType: function() {
+        var rendererFn = Ext.emptyFn;
 
-		switch (this.type) {
-			case 'colour':
-				rendererFn = this.colourRenderer;
-				break;
-			case 'document':
-				rendererFn = this.documentRenderer;
-				break;
-			case 'option':
-				rendererFn = this.optionRenderer;
-				break;
-			case 'date':
-				rendererFn = this.dateRenderer;
-				break;
-			case 'user':
-				rendererFn = this.userRenderer;
-				break;
-			case 'long-text':
-			case 'brief-text':
-				rendererFn = this.simpleRenderer;
-				break;
-		}
+        switch (this.type) {
+            case 'colour':
+                rendererFn = this.colourRenderer;
+                break;
+            case 'document':
+                rendererFn = this.documentRenderer;
+                break;
+            case 'option':
+                rendererFn = this.optionRenderer;
+                break;
+            case 'date':
+                rendererFn = this.dateRenderer;
+                break;
+            case 'user':
+                rendererFn = this.userRenderer;
+                break;
+            case 'long-text':
+            case 'brief-text':
+                rendererFn = this.simpleRenderer;
+                break;
+        }
 
-		this.renderer = rendererFn;
-	},
+        this.renderer = rendererFn;
+    },
 
-	colourRenderer: function(value) {
-		return Ext.String.format('<div style="width: 15px; height: 15px; background-color: {0}; border: 1px solid black;">&nbsp;</div>', value);
-	},
+    colourRenderer: function(value) {
+        if (!value) {
+            value = 'ffffff';
+        }
+        var hex = '#'+value.replace('#', ''); // be sure in true color format;
+        return Ext.String.format('<div style="width: 15px; height: 15px; background-color: {0}; border: 1px solid black;">&nbsp;</div>', hex);
+    },
 
-	documentRenderer: function(value) {
-		return value;
-	},
+    documentRenderer: function(value) {
+        return value;
+    },
 
-	optionRenderer: function(value) {
-		return value;
-	},
+    optionRenderer: function(value) {
+        return value;
+    },
 
-	dateRenderer: function(value) {
-		return value;
-	},
+    dateRenderer: function(value) {
+        return value;
+    },
 
-	userRenderer: function(value) {
-		return value;
-	},
+    userRenderer: function(value) {
+        return value;
+    },
 
-	simpleRenderer: function(value) {
-		return value;
-	}
+    simpleRenderer: function(value) {
+        return value;
+    }
 });
