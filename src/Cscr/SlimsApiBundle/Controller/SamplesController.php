c845d19392048511691638e8598713f0e3e478f0
Stuart Chapman <stubez@users.noreply.github.com>
2015-08-17 10:37:22 +0100

    Remove dummy sample API endpoint #47

--- src/Cscr/SlimsApiBundle/Controller/SamplesController.php
+++ src/Cscr/SlimsApiBundle/Controller/SamplesController.php
@@ -38,48 +38,4 @@ class SamplesController extends FOSRestController
 
         return new FilteredSamplesResponse($results);
     }
-
-    /**
-     * @Rest\Route("/dummy")
-     */
-    public function dummyAction()
-    {
-        $content = <<<HEREDOC
-{
-  "success": true,
-  "data": [
-    {
-      "container": 5,
-      "containerName": "Floor 4",
-      "row": 7,
-      "column": 7,
-      "colour": "#451322",
-      "sampleType": 22,
-      "sampleTypeName": "Mouse Mickey",
-      "name": "Sample Name",
-      "sampleTemplate": 31,
-      "sampleTemplateName": "Template Name",
-      "attributes": [
-        {
-          "id": 12,
-          "label": "City",
-          "type": "text",
-          "value": "Los Angeles"
-        },
-        {
-          "id": 1,
-          "label": "Attached map",
-          "type": "file",
-          "filename": "Manufacturer information.pdf",
-          "mime_type": "application/pdf",
-          "value": "<base 64 encoded file content>"
-        }
-      ]
-    }
-  ]
-}
-HEREDOC;
-
-        return new Response($content);
-    }
 }
