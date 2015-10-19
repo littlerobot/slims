010c5d847360ff9e3c37fe81bd9d60d5b5410998
Stuart Chapman <stubez@users.noreply.github.com>
2015-10-19 10:54:13 +0100

    Exclude activity element from sample instance template response - #68
    The activity is specified in the parent element and was only included in
    the response by accident.

--- src/Cscr/SlimsApiBundle/Response/SampleInstanceTemplateCollectionResponse.php
+++ src/Cscr/SlimsApiBundle/Response/SampleInstanceTemplateCollectionResponse.php
@@ -2,6 +2,10 @@
 
 namespace Cscr\SlimsApiBundle\Response;
 
+use Cscr\SlimsApiBundle\Entity\AbstractSampleInstanceTemplateAttribute;
+use Cscr\SlimsApiBundle\Entity\SampleInstanceTemplate;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Selectable;
 use JMS\Serializer\Annotation as JMS;
 
 class SampleInstanceTemplateCollectionResponse extends ExtJsResponse
@@ -12,4 +16,50 @@ class SampleInstanceTemplateCollectionResponse extends ExtJsResponse
      * @JMS\SerializedName("sample_instance_templates")
      */
     protected $data;
+
+    /**
+     * @param ArrayCollection|SampleInstanceTemplate[] $templates
+     */
+    public function __construct(array $templates)
+    {
+        $this->success = !empty($templates);
+
+        $this->map($templates);
+    }
+
+    /**
+     * @param array|SampleInstanceTemplate[] $templates
+     */
+    private function map(array $templates)
+    {
+        foreach ($templates as $template) {
+            $this->data[] = [
+                'id' => $template->getId(),
+                'name' => $template->getName(),
+                'store' => $this->mapAttributes($template->getStoredAttributes()),
+                'remove' => $this->mapAttributes($template->getRemovedAttributes()),
+            ];
+        }
+    }
+
+    /**
+     * @param Selectable|AbstractSampleInstanceTemplateAttribute[] $attributes
+     *
+     * @return array
+     */
+    private function mapAttributes(Selectable $attributes)
+    {
+        $data = [];
+
+        foreach ($attributes as $attribute) {
+            $data[] = [
+                'id' => $attribute->getId(),
+                'order' => $attribute->getOrder(),
+                'label' => $attribute->getLabel(),
+                'type' => $attribute->getType(),
+            ];
+        }
+
+        return $data;
+    }
 }
